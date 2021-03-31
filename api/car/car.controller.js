4b58db278cd715c030fa9c0f79eea06d46abce11
Arthur-Volkov <bafequan@gmail.com>
2021-03-31 11:44:40 +0300

    after build - by Artur

--- api/car/car.controller.js
+++ api/car/car.controller.js
@@ -69,6 +69,7 @@ async function addCar(req, res) {
 
 async function addTime(req, res) {
     const carId = req.body.carId
+    console.log('req:', req)
     await carService.addTime(carId)
     res.send(carId)
 }
